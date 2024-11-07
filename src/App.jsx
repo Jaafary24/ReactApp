274af7ff8bd2e62bd9cd98d370cc1817ab8821cb
Fred Jaafary <113004580+Jaafary24@users.noreply.github.com>
2024-11-08 00:01:55 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header";
+import Todo from "./components/Todo";
+import "./App.css";
+
+function App() {
+  return (
+    <div classname="App">
+      <Header />
+      <Todo />
+    </div>
+  );
+}
+
+export default App;
